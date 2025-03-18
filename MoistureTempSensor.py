2f7744603adfdbfa9b6404b606ccc240e956fe46
mauvrie <mia.arambulo531@gmail.com>
2025-03-18 16:08:20 -0500

    Update MoistureTempSensor.py

--- MoistureTempSensor.py
+++ MoistureTempSensor.py
@@ -1,245 +1,86 @@
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
-
-import time
-
-import board
-
-from adafruit_seesaw.seesaw import Seesaw
-
-i2c_bus = board.I2C()  # uses board.SCL and board.SDA
-# i2c_bus = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
-
-ss = Seesaw(i2c_bus, addr=0x36)
 
 
+//CURRENTLY IN C++
+// Include the Adafruit Seesaw library which provides easy access to the Seesaw sensor
+#include "Adafruit_seesaw.h"
 
+// Create an instance of the Adafruit_seesaw class to interact with the sensor
+Adafruit_seesaw ss;
 
-MOISTURE_THRESHOLD = 740 
-'''
-200(very dry)-2000(very) = 1800
-1800 * 0.3 = 540
-540 + 200 = 740 (threshold for the "30% or less" humidity)
-'''
-TEMP_THRESHOLD = 30
-WIND_THRESHOLD = 
-single_trial = True
+const int MOISTURE_THRESHOLD = 740; // Threshold for moisture (below this = dry soil)
 
-while single_trial:
-    #counts how many conditions are met
-    condition_count = 0
+//200(very dry)-2000(very wet) = 1800
+//1800 * 0.3 = 540
+//540 + 200 = 740 (threshold for the "30% or less" humidity)
 
-    # read moisture level through capacitive touch pad
-    touch = ss.moisture_read()
-
-    # read temperature from the temperature sensor
-    temp = ss.get_temp()
-
-    #Moisture condition
-    if touch <= MOISTURE_THRESHOLD:
-        condition_count += 1
-    #Temperature condition
-    if temp >= TEMP_THRESHOLD:
-        condition_count += 1
-    #Wind/Time of flight condition
-
-
-    #to stop loop and give the values for 1 trial
-    single_trial = False
+const int TEMP_THRESHOLD = 30;     // Threshold for temperature (above this = hot)
 
 
 
-    print("temp: " + str(temp) + "  moisture: " + str(touch))
-    time.sleep(1)
+void setup() {
+  // Start serial communication with a baud rate of 115200 for debugging
+  Serial.begin(115200);
+
+  // Print a message to indicate the start of the program
+  Serial.println("seesaw Soil Sensor example!");
+  
+  // Initialize the seesaw sensor with the I2C address of 0x36
+  // If initialization fails, print an error and halt the program
+  if (!ss.begin(0x36)) {
+    Serial.println("ERROR! seesaw not found");
+    while(1) delay(1);  // Infinite loop to halt the program if sensor is not found
+  } else {
+    // If the sensor is found, print a success message and the sensor version
+    Serial.print("seesaw started! version: ");
+    Serial.println(ss.getVersion(), HEX);  // Print the sensor's firmware version in hexadecimal format
+  }
+}
+
+void loop() {
+  int condition_count = 0;  // To count how many conditions are met (must equal 2 for both moisture and temp)
+
+  // Get the current temperature reading from the sensor in Celsius
+  float temp = ss.getTemp(); 
 
 
-#count how many conditions are met to adjust the angle in the gauge
-
-if condition_count == 3:
-    #code here for the gauge
-elif condition_count == 2:
-    #code here for the gauge
-elif condition_count == 1:
-    #code here 
-else: #if 0 conditions are met
-    #code here
+  // Read the capacitive touch value (soil moisture level)
+  // The first parameter (0) refers to the first capacitive touch pin
+  uint16_t touch = ss.touchRead(0);
 
 
+   // Moisture condition: check if moisture is below or equal to the threshold
+    if (touch <= MOISTURE_THRESHOLD) {
+      condition_count++;  // Increment if moisture is below threshold (dry soil)
+
+      // Print the capacitive touch reading ONLY IF IT MEETS CONDITION, which is a measure of the soil moisture level 
+      Serial.print("Moisture condition met: ");
+      Serial.println(touch);
+    }
+
+    // Temperature condition: check if the temperature is above or equal to the threshold
+    if (temp >= TEMP_THRESHOLD) {
+      condition_count++;  // Increment if temperature is higher than threshold (hot)
+      
+      // Print the temperature value to the serial monitor ONLY IF IT MEETS CONDITION
+      Serial.print("Temperature condition met: ");
+      Serial.println(temp);
+      Serial.println("*C");
+    }
+
+  // Print the temperature value to the serial monitor (CAN BE REMOVED)
+  Serial.print("Temperature: "); Serial.print(temp); Serial.println("*C");
+  
+  // Print the capacitive touch reading, which is a measure of the soil moisture level (CAN BE REMOVED)
+  Serial.print("Capacitive: "); Serial.println(touch);
+
+  // Delay for 100 milliseconds before repeating the loop
+  delay(100);
+
+
+}
 
 
 
 
 
 
-
-# Byte-compiled / optimized / DLL files
-__pycache__/
-*.py[cod]
-*$py.class
-
-# C extensions
-*.so
-
-# Distribution / packaging
-.Python
-build/
-develop-eggs/
-dist/
-downloads/
-eggs/
-.eggs/
-lib/
-lib64/
-parts/
-sdist/
-var/
-wheels/
-share/python-wheels/
-*.egg-info/
-.installed.cfg
-*.egg
-MANIFEST
-
-# PyInstaller
-#  Usually these files are written by a python script from a template
-#  before PyInstaller builds the exe, so as to inject date/other infos into it.
-*.manifest
-*.spec
-
-# Installer logs
-pip-log.txt
-pip-delete-this-directory.txt
-
-# Unit test / coverage reports
-htmlcov/
-.tox/
-.nox/
-.coverage
-.coverage.*
-.cache
-nosetests.xml
-coverage.xml
-*.cover
-*.py,cover
-.hypothesis/
-.pytest_cache/
-cover/
-
-# Translations
-*.mo
-*.pot
-
-# Django stuff:
-*.log
-local_settings.py
-db.sqlite3
-db.sqlite3-journal
-
-# Flask stuff:
-instance/
-.webassets-cache
-
-# Scrapy stuff:
-.scrapy
-
-# Sphinx documentation
-docs/_build/
-
-# PyBuilder
-.pybuilder/
-target/
-
-# Jupyter Notebook
-.ipynb_checkpoints
-
-# IPython
-profile_default/
-ipython_config.py
-
-# pyenv
-#   For a library or package, you might want to ignore these files since the code is
-#   intended to run in multiple environments; otherwise, check them in:
-# .python-version
-
-# pipenv
-#   According to pypa/pipenv#598, it is recommended to include Pipfile.lock in version control.
-#   However, in case of collaboration, if having platform-specific dependencies or dependencies
-#   having no cross-platform support, pipenv may install dependencies that don't work, or not
-#   install all needed dependencies.
-#Pipfile.lock
-
-# UV
-#   Similar to Pipfile.lock, it is generally recommended to include uv.lock in version control.
-#   This is especially recommended for binary packages to ensure reproducibility, and is more
-#   commonly ignored for libraries.
-#uv.lock
-
-# poetry
-#   Similar to Pipfile.lock, it is generally recommended to include poetry.lock in version control.
-#   This is especially recommended for binary packages to ensure reproducibility, and is more
-#   commonly ignored for libraries.
-#   https://python-poetry.org/docs/basic-usage/#commit-your-poetrylock-file-to-version-control
-#poetry.lock
-
-# pdm
-#   Similar to Pipfile.lock, it is generally recommended to include pdm.lock in version control.
-#pdm.lock
-#   pdm stores project-wide configurations in .pdm.toml, but it is recommended to not include it
-#   in version control.
-#   https://pdm.fming.dev/latest/usage/project/#working-with-version-control
-.pdm.toml
-.pdm-python
-.pdm-build/
-
-# PEP 582; used by e.g. github.com/David-OConnor/pyflow and github.com/pdm-project/pdm
-__pypackages__/
-
-# Celery stuff
-celerybeat-schedule
-celerybeat.pid
-
-# SageMath parsed files
-*.sage.py
-
-# Environments
-.env
-.venv
-env/
-venv/
-ENV/
-env.bak/
-venv.bak/
-
-# Spyder project settings
-.spyderproject
-.spyproject
-
-# Rope project settings
-.ropeproject
-
-# mkdocs documentation
-/site
-
-# mypy
-.mypy_cache/
-.dmypy.json
-dmypy.json
-
-# Pyre type checker
-.pyre/
-
-# pytype static type analyzer
-.pytype/
-
-# Cython debug symbols
-cython_debug/
-
-# PyCharm
-#  JetBrains specific template is maintained in a separate JetBrains.gitignore that can
-#  be found at https://github.com/github/gitignore/blob/main/Global/JetBrains.gitignore
-#  and can be added to the global gitignore or merged into this file.  For a more nuclear
-#  option (not recommended) you can uncomment the following to ignore the entire idea folder.
-#.idea/
-
-# PyPI configuration file
-.pypirc
